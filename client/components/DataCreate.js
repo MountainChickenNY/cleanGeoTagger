3c307261e6d621db796c7e099faf3a5fc6313494
Jeffrey Schrock <rhythmmagi@gmail.com>
2018-06-06 18:56:17 +0000

    finished styles for feed

--- client/components/DataCreate.js
+++ client/components/DataCreate.js
@@ -25,9 +25,9 @@ class DataCreate extends Component {
     if(tagCount) {
       let tagsElems = document.getElementsByClassName('tags');
       for(let i = 0; i < tagsElems.length; i++) {
-          tags.push(tagsElems[i].value);
+        tags.push(tagsElems[i].value);
       }
-    } 
+    }
     data.name = name;
     data.tags = tags;
 
@@ -41,10 +41,21 @@ class DataCreate extends Component {
   }
 
   handleClickPlus = () => {
-    let updateTagCount = this.state.tagCount+1;
-    this.setState({
-      tagCount: updateTagCount
-    });
+    if(this.state.tagCount < 4){
+      let updateTagCount = this.state.tagCount+1;
+      this.setState({
+        tagCount: updateTagCount
+      }); 
+    }
+  }
+  
+  handleClickMin = () => {
+    if(this.state.tagCount >= 1){
+      let updateTagCount = this.state.tagCount-1; 
+      this.setState({
+        tagCount: updateTagCount
+      });
+    }
   }
   
   render(){
@@ -52,19 +63,21 @@ class DataCreate extends Component {
     let moreTags = [];
     if(tagCount) {
       for(let i = 0; i < tagCount; i++) {
-        moreTags.push(<input className="tags" type="text" name = {"tag"+i+1} placeholder="tag" key={"tag"+i+1}/>);
+        moreTags.push(<input className="tags form-control" type="text" name = {"tag"+i+1} placeholder="tag" key={"tag"+i+1}/>);
       }
     }
     return (
       <div className="input-group mb-3">
         <form className="form-group">
-          <input className="form-control" type="text" name="name" placeholder="name" value={this.state.name} onChange={this.handleChange} />
+          <input className="form-control" type="text" name="name" placeholder="example: Codesmith NY" value={this.state.name} onChange={this.handleChange} />
           <br />
+          <a>Save to a tag!</a>
           <input className="tag form-control" type="text" name="tag" placeholder="tag" value={this.state.tag} onChange={this.handleChange} />
           {moreTags}
-          {/*<button className="btn" onClick={this.handleClickPlus}>+</button>*/}
           <br />
-          <button type="submit" className="btn" onClick={this.handleClickSave}>SAVE</button>
+          <a className="btn btn-secondary ml-1" onClick={this.handleClickPlus}>Add Tag</a>
+          <a className="btn btn-secondary ml-1" onClick={this.handleClickMin}>Remove Tag</a>
+          <button type="submit" className="btn btn-secondary ml-1" onClick={this.handleClickSave}>SAVE</button>
         </form>
       </div>
     );
